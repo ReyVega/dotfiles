5ff0489a1fd6b5c624a0f360ef79bdda6ce9af5d
Rey Vega <reynaldovega2010@hotmail.com>
2024-06-23 09:44:58 -0600

    fix: updates gitsigns plugin config

--- .config/nvim/lua/plugins/ui/gitsigns.lua
+++ .config/nvim/lua/plugins/ui/gitsigns.lua
@@ -15,34 +15,19 @@ return {
 		gitsigns.setup({
 			signs = {
 				add = {
-					hl = "GitSignsAdd",
 					text = "│",
-					numhl = "GitSignsAddNr",
-					linehl = "GitSignsAddLn",
 				},
 				change = {
-					hl = "GitSignsChange",
 					text = "│",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
 				},
 				delete = {
-					hl = "GitSignsDelete",
 					text = "_",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
 				},
 				topdelete = {
-					hl = "GitSignsDelete",
 					text = "‾",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
 				},
 				changedelete = {
-					hl = "GitSignsChange",
 					text = "~",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
 				},
 			},
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
@@ -58,7 +43,6 @@ return {
 				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
 				delay = 1000,
 			},
-			current_line_blame_formatter_opts = { relative_time = false },
 			sign_priority = 6,
 			update_debounce = 100,
 			status_formatter = nil, -- Use default
@@ -72,7 +56,6 @@ return {
 				col = 1,
 			},
 			diff_opts = { internal = true }, -- If vim.diff or luajit is present
-			yadm = { enable = false },
 		})
 	end,
 }
